0f09e9ccac037b02ec7ce0e1dd4223629b2bcb3e
suixx <2967843382@qq.com>
2018-06-08 16:53:07 +0800

    第三次提交

---
+++ src/main/java/com/baizhi/entity/User.java
@@ -0,0 +1,9 @@
+package com.baizhi.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String id;
+    private String name;
+    private String password;
+}
